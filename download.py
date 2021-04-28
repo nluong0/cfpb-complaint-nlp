db76b2933f15b15fae316141fe41598ac84e6825
nluong0 <64280847+nluong0@users.noreply.github.com>
2021-04-28 12:52:28 -0500

    Added functions to create complainst_list.gz

--- download.py
+++ download.py
@@ -1,35 +1,14 @@
-import pandas as pd
-import subprocess
-from datetime import date
+import json
+import gzip
 
-#import requests
-# def go():
-#     url = 'https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/?size=2000&no_aggs=true&has_narrative=true' 
-#     r = requests.get(url) 
-#     complaint_list = [i['_source'] for i in r.json()['hits']['hits']] 
-#     df = pd.DataFrame(complaint_list)
-#     total_matches = r.json()['hits']['total'] 
-#     assert total_matches == len(df)
+def load_data():
+    complaints = None
+    with open('complaints.json') as file: 
+        data = json.load(file) 
+        complaints = [(i['_id'], i['_source']['complaint_what_happened']) for i in data]
 
-def download_complaints():
-    try:
-        subprocess.call(['sh', 'download.sh'])
-    except Exception as e:
-        print(f'Failed download: {e}')
+    with gzip.open('complaints_list.gz', 'wb') as f:
+        f.write(complaints)
 
-def go():
-    with open('complaints.json') as f:
-        old_complaints = json.load(f)
-    old_num_complaints = len(old_complaints)
-
-    download_complaints()
-    
-    with open('complaints.json') as f:
-        new_complaints = json.load(f)
-    new_num_complaints = len(new_complaints)
-
-    print(f'{new_num_complaints - old_num_complaints} complaints with narrative have been sumitted.')
-
-                            
 if __name__ == '__main__':
-    go()
+    load_data()()
